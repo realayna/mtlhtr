efcad07138171a3685d6c384cd927ab25e886bfd
realayna <ayna1204@gmail.com>
2023-09-09 20:25:20 +0200

    forecast

--- main.cpp
+++ main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <map>
+#include <string>
+#include <chrono>
+using namespace std::chrono_literals;
 
-
-void function1(char symbol)
+void refreshForecast(std::map<std::string, int> forecastmap)
 {
-    for (int i=0; i<200; i++)
+    while (true)
     {
-        std::cout<<symbol;
+        for (auto item : forecastmap)
+        {
+            item.second++;
+            std::cout << item.first << " - " << item.second << std::endl;
+        }
 
-    }
-}
-void function2()
-{
-    for (int i=0; i<200; i++)
-    {
-        std::cout<<"-";
-        
+        std::this_thread::sleep_for(2000ms);
     }
 }
 
 int main()
 {
-    std::thread worker1(function1, 'o');
-    std::thread worker2(function2);
-    worker1.join();
-    worker2.join();
-
-
+    std::map<std::string, int> forecastmap = {
+        {"New York", 15},
+        {"Mumbai", 20},
+        {"Berlin", 18}};
 }
