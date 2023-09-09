9be4d643d5bb2d7950eefac8c0e4c172c39633b9
realayna <ayna1204@gmail.com>
2023-09-09 20:07:23 +0200

    single thread

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <thread>
+
+
+void function1()
+{
+    for (int i=0; i<200; i++)
+    {
+        std::cout<<"+ ";
+
+    }
+}
+void function2()
+{
+    for (int i=0; i<200; i++)
+    {
+        std::cout<<"- ";
+        
+    }
+}
 
 int main()
 {
-    std::cout<<"hello world";
+    function1();
+    function2();
+
 }
